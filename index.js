afc707c8a417f6a1ea09a7420504a2c2b1f28282
jake.c.rummel@gmail.com <jake.c.rummel@gmail.com>
2018-08-21 21:14:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // add solution here
+
+
+function theBeatlesPlay(musicians,instruments) {
+  var theBeatlesPlay = []
+  
+  for (let i = 0; i < 4; i++) {
+  theBeatlesPlay.push(`${musicians[i]} plays ${instruments[i]}`) 
+  }
+return theBeatlesPlay }  
+
+function johnLennonFacts(facts) {
+  var exclamationPoint = [];
+  var i = 0;
+  while (i < 4) {
+    exclamationPoint.push(`${facts[i]}!!!`); i++; 
+  }
+  return exclamationPoint;
+}
